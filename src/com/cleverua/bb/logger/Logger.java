863289c1fcbeb515044ceb8b430d187bfcb71fee
Vitaliy Khudenko <vitaliy.khudenko@gmail.com>
2010-02-10 12:14:55 +0200

    fixes identation from tabs to spaces

--- src/com/cleverua/bb/logger/Logger.java
+++ src/com/cleverua/bb/logger/Logger.java
@@ -18,103 +18,103 @@ import net.rim.device.api.system.DeviceInfo;
  * In order to turn it off just modify LOG_ENABLED boolean constant and rebuild the project.
  */
 public class Logger {
-	
-	// enable if having trouble debugging
-	public static final boolean LOG_ENABLED = true;
-	
-	public static final String LOG_FILE_NAME = "application.log";
-	public static final String LOG_FILE 	 = "file:///SDCard/" + LOG_FILE_NAME;
-	// or to put in DeviceMemory: "file:///store/home/user/" + LOG_FILE_NAME;
-	
-	private static final String DEBUG 		= "DEBUG";
-	private static final String DATE_START 	= " [ ";
-	private static final String DATE_END 	= " ]: ";
-	private static final String DELIMITER_1 = " : ";
-	private static final String DELIMITER_2 = ": ";
-	private static final String NL 			= "\n";
-	
-	private static final int MAX_LOG_FILE_SIZE = 10 * 1024 * 1024; /* 10MB */
-	
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	
-	public synchronized static void debug(String msg) {
-		
-		// always write log file if on simulator 
-		if (DeviceInfo.isSimulator()) {
-			writeToFile(getMessage(msg));
-			return;
-		}
-		
-		// if on real device do logging only if logger is enabled globally (LOG_ENABLED)
-		if (LOG_ENABLED) {
-			writeToFile(getMessage(msg));
-		}
-	}
-	
-	public synchronized static void debug(Object o, String msg) {
-		debug(new StringBuffer(o.getClass().toString()).append(DELIMITER_2).append(msg).toString());
-	}
-	
-	private static String getMessage(String msg) {
-		return new StringBuffer(DEBUG)
-			.append(DATE_START).append(DATE_FORMAT.format(new Date())).append(DATE_END)
-			.append(Thread.currentThread().toString()).append(DELIMITER_1)
-			.append(msg).append(NL).toString();
-	}
-	
-	private synchronized static void writeToFile(String msg) {
-		FileConnection fc = null;
-		OutputStream out = null;
-		OutputStreamWriter writer = null;
-		try {
-			fc = (FileConnection) Connector.open(LOG_FILE, Connector.READ_WRITE);
-			if (!fc.exists()) {
-				fc.create();
-			}
+
+    // enable if having trouble debugging
+    public static final boolean LOG_ENABLED = true;
+
+    public static final String LOG_FILE_NAME = "application.log";
+    public static final String LOG_FILE      = "file:///SDCard/" + LOG_FILE_NAME;
+    // or to put in DeviceMemory: "file:///store/home/user/" + LOG_FILE_NAME;
+
+    private static final String DEBUG       = "DEBUG";
+    private static final String DATE_START  = " [ ";
+    private static final String DATE_END    = " ]: ";
+    private static final String DELIMITER_1 = " : ";
+    private static final String DELIMITER_2 = ": ";
+    private static final String NL          = "\n";
+
+    private static final int MAX_LOG_FILE_SIZE = 10 * 1024 * 1024; /* 10MB */
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public synchronized static void debug(String msg) {
+
+        // always write log file if on simulator 
+        if (DeviceInfo.isSimulator()) {
+            writeToFile(getMessage(msg));
+            return;
+        }
+
+        // if on real device do logging only if logger is enabled globally (LOG_ENABLED)
+        if (LOG_ENABLED) {
+            writeToFile(getMessage(msg));
+        }
+    }
+
+    public synchronized static void debug(Object o, String msg) {
+        debug(new StringBuffer(o.getClass().toString()).append(DELIMITER_2).append(msg).toString());
+    }
+
+    private static String getMessage(String msg) {
+        return new StringBuffer(DEBUG)
+        .append(DATE_START).append(DATE_FORMAT.format(new Date())).append(DATE_END)
+        .append(Thread.currentThread().toString()).append(DELIMITER_1)
+        .append(msg).append(NL).toString();
+    }
+
+    private synchronized static void writeToFile(String msg) {
+        FileConnection fc = null;
+        OutputStream out = null;
+        OutputStreamWriter writer = null;
+        try {
+            fc = (FileConnection) Connector.open(LOG_FILE, Connector.READ_WRITE);
+            if (!fc.exists()) {
+                fc.create();
+            }
 
             if (fc.fileSize() > MAX_LOG_FILE_SIZE && LOG_ENABLED) {
                 fc.delete();
                 fc.create();
             }
 
-			out = fc.openOutputStream(fc.availableSize());
-			writer = new OutputStreamWriter(out);
-			writer.write(msg);
-			writer.flush();
+            out = fc.openOutputStream(fc.availableSize());
+            writer = new OutputStreamWriter(out);
+            writer.write(msg);
+            writer.flush();
 
-		} catch (IOException e) {
-			System.out.println("Failed to write to log file, " + e);
-		} finally {
-			safelyCloseStream(writer);
-			safelyCloseStream(out);
-			safelyCloseStream(fc);
-		}
-	}
-	
-	private static void safelyCloseStream(OutputStream stream) {
-		if (stream != null) {
-			try {
-				stream.close();
-				stream = null;
-			} catch (Exception e) { /* that's ok */ }
-		}
-	}
-	
-	private static void safelyCloseStream(FileConnection stream) {
-		if (stream != null) {
-			try {
-				stream.close();
-				stream = null;
-			} catch (Exception e) { /* that's ok */ }
-		}
-	}
-	
-	private static void safelyCloseStream(OutputStreamWriter stream) {
-		if (stream != null) {
-			try {
-				stream.close();
-				stream = null;
-			} catch (Exception e) { /* that's ok */ }
-		}
-	}
+        } catch (IOException e) {
+            System.out.println("Failed to write to log file, " + e);
+        } finally {
+            safelyCloseStream(writer);
+            safelyCloseStream(out);
+            safelyCloseStream(fc);
+        }
+    }
+
+    private static void safelyCloseStream(OutputStream stream) {
+        if (stream != null) {
+            try {
+                stream.close();
+                stream = null;
+            } catch (Exception e) { /* that's ok */ }
+        }
+    }
+
+    private static void safelyCloseStream(FileConnection stream) {
+        if (stream != null) {
+            try {
+                stream.close();
+                stream = null;
+            } catch (Exception e) { /* that's ok */ }
+        }
+    }
+
+    private static void safelyCloseStream(OutputStreamWriter stream) {
+        if (stream != null) {
+            try {
+                stream.close();
+                stream = null;
+            } catch (Exception e) { /* that's ok */ }
+        }
+    }
 }
